1c0e5b52a095370d2589560dce00702509e07be6
fultlin <ugolok.kita@gmail.com>
2024-02-13 13:22:10 +0300

    lab2 Form

---
+++ lab2/back.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <textarea name="" id="" cols="100" rows="10">
+        <?php
+            print_r(get_headers('https://online.mospolytech.ru/mod/assign/view.php?id=280561'))
+        ?>
+    </textarea>
+    <a href='index.html'>Назад</a>
+</body>
+</html>
